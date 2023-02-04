6e7bc443365e93d2d6a77018d249c8cc3e71092f
Silvio Endruhn <silvio@endruhn.de>
2023-02-04 20:52:11 +0100

    add function addDimensionsOfImages

--- Images_Without_Dimensions.js
+++ Images_Without_Dimensions.js
@@ -1,9 +1,13 @@
-function ready(fn){var d=document;(d.readyState=='loading')?d.addEventListener('DOMContentLoaded',fn):fn();}
+function ready(fn){let d=document;(d.readyState=='loading')?d.addEventListener('DOMContentLoaded',fn):fn();}
 
-ready(function(){
-    let images = document.getElementsByTagName("img");
+function addDimensionsOfImages(){
+    const images = document.getElementsByTagName("img");
     for (let i=0; i<images.length; i++) {
         images[i].setAttribute("width", images[i].width);
         images[i].setAttribute("height", images[i].height);
     };
+}
+
+ready(function(){
+    addDimensionsOfImages();
 });
